d7324db5b57ae0d19c9f281376521e36b665d3e2
Alexander Sosedkin <asosedkin@redhat.com>
2023-05-29 10:10:44 +0200

    __init__.py: add SPDX info

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: {{ cookiecutter.initial_year }} {{ cookiecutter.full_name }} <{{ cookiecutter.email }}>
+# SPDX-License-Identifier: {{ cookiecutter.license_spdx }}
+
 """
 {{ cookiecutter.project_name }}.
 
